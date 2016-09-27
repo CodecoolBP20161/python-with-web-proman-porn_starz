664e282e6e874e7a7bac651956e6d7dec1d305be
vogronicspatrik <vogronicspatrik@gmail.com>
2016-09-27 13:30:45 +0200

    first US done

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def start():
+    return render_template('home.html')
+
+
+
+
+
+
+if '__main__' == __name__:
+    app.run(debug=True)
---
+++ static/js/home.js
@@ -0,0 +1,1 @@
+var example = prompt("mukodj!!!!???: ")
