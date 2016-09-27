6ed1c06118a4fcb3ccd7b03abe65c3dbf60b5857
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-09-27 22:24:17 +0200

    comment added

--- static/js/display.js
+++ static/js/display.js
@@ -1,6 +1,8 @@
 
 var initialLoading = function() {
+    // This line will be replaced by data loader function from the proxy, we will need id as well
     var titles = ['Project1', 'Project2', 'Project3', 'Project4', 'Project5'];
+    // ---------------------------------------------------------------------------------------
     var list = document.createElement('ul');
     titles.forEach(function (item, index, array) {
         var board = document.createElement('li');
